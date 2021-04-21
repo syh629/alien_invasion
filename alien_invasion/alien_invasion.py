626abe8158847c6e440fa8bd60d1c8acaa324301
syh <syhdsg@sjtu.edu.cn>
2021-04-21 10:48:39 +0800

    4\21

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -21,14 +21,14 @@ class AlienInvasion:
         pygame.display.set_caption('Alien Invasion')   #设置游戏标题
 
         self.ship = Ship(self)  #因为Ship还有个参数ai_game 这里把AlienInvasion实例赋给ai_game
-        self.bullet = pygame.sprite.Group()
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         '''开始游戏的主循环'''
         while True:
             self._check_events()
             self.ship.update()
-            self.bullet.update()
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -61,14 +61,25 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         '''创建一颗子弹，并将其加入编组bullets中'''
-        new_bullet = Bullet(self)
-        self.bullet.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        '''更新子弹的位置并删除消失的子弹'''
+        #更新子弹的位置
+        self.bullets.update()
+
+        #删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         '''更新屏幕上的图像，并切换到新屏幕'''
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-        for bullet in self.bullet.sprites():        #bullet.sprites()返回一个列表，包含编组bullets中所有的精灵
+        for bullet in self.bullets.sprites():        #bullets.sprites()返回一个列表，包含编组bullets中所有的精灵
             bullet.draw_bullet()
 
         #让最近绘制的屏幕可见
