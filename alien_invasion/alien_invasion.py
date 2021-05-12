a146947cc72eb73cc759d3117b3d1619d30a70d4
syh <syhdsg@sjtu.edu.cn>
2021-05-12 19:43:32 +0800

    5/12

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -75,29 +75,34 @@ class AlienInvasion:
 
     def _ship_hit(self):
         '''响应飞船被外星人撞到'''
+        if self.stats.ships_left >0:
+            #将ship_left减一
+            self.stats.ships_left -= 1
 
-        #将ship_left减一
-        self.stats.ships_left -= 1
+            #清空余下的外星人和子弹
+            self.aliens.empty()
+            self.bullets.empty()
 
-        #清空余下的外星人和子弹
-        self.aliens.empty()
-        self.bullets.empty()
+            #创建一群新的外星人，并将飞船放在屏幕中央
+            self._create_fleet()
+            self.ship.center_ship()
 
-        #创建一群新的外星人，并将飞船放在屏幕中央
-        self._create_fleet()
-        self.ship.center_ship()
+            #暂停
+            sleep(0.5)
 
-        #暂停
-        sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def run_game(self):
         '''开始游戏的主循环'''
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
-            self._update_screen()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                self._update_screen()
 
     def _check_events(self):
         '''相应按键和鼠标事件'''
@@ -163,6 +168,18 @@ class AlienInvasion:
         #检测外星人和飞船之间的碰撞
         if pygame.sprite.spritecollideany(self.ship,self.aliens):
             self._ship_hit()
+
+        #检查是否有外星人到达屏幕底端
+        self._check_aliens_bottom()
+
+    def _check_aliens_bottom(self):
+        '''检查是否有外星人到达屏幕底端'''
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                #像飞船被撞到一样处理
+                self._ship_hit()
+                break
 
     def _update_screen(self):
         '''更新屏幕上的图像，并切换到新屏幕'''
